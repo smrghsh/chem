a40f0e8846270a7c32eb398e334e45fcca926e53
Samir Ghosh <hello@samir.tech>
2024-03-30 14:39:59 -0700

    add: clear?

--- src/Experience/Renderer.js
+++ src/Experience/Renderer.js
@@ -15,6 +15,7 @@ export default class Renderer {
     this.instance = new THREE.WebGLRenderer({
       canvas: this.canvas,
       antialias: true,
+      alpha: true,
     });
     this.instance.setSize(this.sizes.width, this.sizes.height);
     this.instance.setPixelRatio(Math.min(this.sizes.pixelRatio, 2));
