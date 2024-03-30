dcb7e9b5129da0bbf7ce8540fc682ab3e8fd651c
Samir Ghosh <hello@samir.tech>
2024-03-30 13:37:01 -0700

    add: this

--- src/Experience/Hands.js
+++ src/Experience/Hands.js
@@ -8,6 +8,7 @@ export default class Hands {
     this.experience = new Experience();
     this.scene = this.experience.scene;
     this.renderer = this.experience.renderer;
+    this.handModelFactory = new XRHandModelFactory();
     // this.controller1 = this.renderer.instance.xr.getController(0);
     // this.controller2 = this.renderer.instance.xr.getController(1);
     // this.controllerGrip1 = this.renderer.instance.xr.getControllerGrip(0);
@@ -37,9 +38,9 @@ export default class Hands {
     // scene.add(controllerGrip1);
 
     this.hand1 = this.renderer.xr.getHand(0);
-    this.hand1.add(handModelFactory.createHandModel(hand1));
+    this.hand1.add(this.handModelFactory.createHandModel(this.hand1));
 
-    this.scene.add(hand1);
+    this.scene.add(this.hand1);
 
     // Hand 2
     // controllerGrip2 = this.renderer.xr.getControllerGrip(1);
@@ -49,7 +50,7 @@ export default class Hands {
     // scene.add(controllerGrip2);
 
     this.hand2 = this.renderer.xr.getHand(1);
-    this.hand2.add(handModelFactory.createHandModel(hand2));
-    this.scene.add(hand2);
+    this.hand2.add(this.handModelFactory.createHandModel(this.hand2));
+    this.scene.add(this.hand2);
   }
 }
